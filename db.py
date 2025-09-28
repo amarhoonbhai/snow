06d493e3bf88e3d3f4a0ffddb06644d93abe3636
अमर <120897603+amarhoonbhai@users.noreply.github.com>
2025-09-28 19:51:24 +0530

    Delete db.py

--- db.py
+++
@@ -1,7 +0,0 @@
-from tinydb import TinyDB, Query
-
-db = TinyDB('../spinauth/accounts.json')  # Adjust path if needed
-accounts_table = db.table("accounts")
-
-def get_user_accounts(user_id):
-    return accounts_table.search(Query().user_id == user_id)
